8c8a5a5db945199fe577261aa3718a7a91efa017
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2024-03-26 18:30:43 -0500

    add formatters

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -5,11 +5,31 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+import db from '@/db/db'
+import { formatCurrency, formatNumber } from '@/lib/formatters'
 
-export default function AdminDashboard() {
+async function getSalesData() {
+  const data = await db.order.aggregate({
+    _sum: { pricePaidCents: true },
+    _count: true,
+  })
+
+  return {
+    amount: (data._sum.pricePaidCents || 0) / 100,
+    numberOfSales: data._count,
+  }
+}
+
+export default async function AdminDashboard() {
+  const salesData = await getSalesData()
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4S">
-      <DashboardCard title="Sales" subtitle="Test" body="Body" />
+      <DashboardCard
+        title="Sales"
+        subtitle={formatNumber(salesData.numberOfSales)}
+        body={formatCurrency(salesData.amount)}
+      />
     </div>
   )
 }
