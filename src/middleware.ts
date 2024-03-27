d22b63407cf7e6f9491ff8eb86301c2b51828ab7
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2024-03-27 01:19:09 -0500

    add admin auth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
+import { isValidPassword } from './lib/isValidPassword'
 
 export async function middleware(req: NextRequest) {
   if ((await isAuthenticated(req)) === false) {
@@ -10,7 +11,22 @@ export async function middleware(req: NextRequest) {
 }
 
 async function isAuthenticated(req: NextRequest) {
-  return Promise.resolve(false)
+  const authHeader =
+    req.headers.get('authorization') || req.headers.get('Authorization')
+
+  if (authHeader == null) return false
+
+  const [username, password] = Buffer.from(authHeader.split(' ')[1], 'base64')
+    .toString()
+    .split(':')
+
+  return (
+    username === process.env.ADMIN_USERNAME &&
+    (await isValidPassword(
+      password,
+      process.env.HASHED_ADMIN_PASSWORD as string
+    ))
+  )
 }
 
 export const config = {
