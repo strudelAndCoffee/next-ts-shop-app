4d1a4f1d51f8b83e37ec9435901a7749bb62f69f
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2024-03-27 18:08:04 -0500

    finish purchase page, form, payment success/failure

--- src/app/(customerFacing)/products/[id]/purchase/page.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -1,5 +1,9 @@
 import db from '@/db/db'
 import { notFound } from 'next/navigation'
+import Stripe from 'stripe'
+import { CheckoutForm } from './_components/CheckoutForm'
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 
 export default async function PurchasePage({
   params: { id },
@@ -9,5 +13,19 @@ export default async function PurchasePage({
   const product = await db.product.findUnique({ where: { id } })
   if (product == null) return notFound()
 
-  return <h1></h1>
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: product.priceInCents,
+    currency: 'USD',
+    metadata: { productId: product.id },
+  })
+
+  if (paymentIntent.client_secret == null)
+    throw Error('Stripe failed to create payment intent.')
+
+  return (
+    <CheckoutForm
+      product={product}
+      clientSecret={paymentIntent.client_secret}
+    />
+  )
 }
